bb0106c35624a75f490fc80fd3b9d141ba92ec0a
Prabal Pratap Singh <prabalparihar95@gmail.com>
2022-11-24 13:46:12 +0530

    sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/Sidebar';
+import Card from './components/Card';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <>
+    {/* <Sidebar/>
+    <Card/>
+     */}
+     <Navbar/>
+  </>
   );
 }
 
